1ad7280efb50d5d15a8fe0a88dedbcd7e213a511
YeHtet1001 <java17springboot@gmail.com>
2025-05-03 23:20:41 +0630

    uploaded beans.xml

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,23 @@
 package org.example;
 
 import org.example.entity.Employee;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Employee emp1 = new Employee( 1 , "Noto");
-
+        Employee emp1 = new Employee();
+        System.out.println("Not Spring Boot Application Context");
         System.out.println("Hello , "+ emp1.toString() + ".");
+
+        BeanFactory beanFactory = new FileSystemXmlApplicationContext("Beans.xml");
+
+        Employee employee = (Employee) beanFactory.getBean("employee");
+
+        System.out.println("Spring With bean.xml");
+        System.out.println(employee.toString());
 
     }
 
--- src/main/java/org/example/entity/Employee.java
+++ src/main/java/org/example/entity/Employee.java
@@ -1,5 +1,6 @@
 package org.example.entity;
 
+
 public class Employee {
 
     private int id;
