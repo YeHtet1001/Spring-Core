a3958f2a79d7f4a498d2d4ff1f4040ff9273cf6a
YeHtet1001 <java17springboot@gmail.com>
2025-05-03 22:57:21 +0630

    upload new

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,14 @@
 package org.example;
 
+import org.example.entity.Employee;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World"+".");
+        Employee emp1 = new Employee( 1 , "Noto");
+
+        System.out.println("Hello , "+ emp1.toString() + ".");
 
     }
 
